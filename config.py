4f14045018dd08d4546264fbd814c0acb93ff07a
kanish-ai <jokergaming35440@gmail.com>
2025-04-28 11:03:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -93,8 +93,8 @@ autoclean = []
 confirmer = {}
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://files.catbox.moe/t0t93h.jpg")
-PING_IMG_URL = getenv("PING_IMG_URL", "https://files.catbox.moe/lbl62s.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://files.catbox.moe/ruy1nl.jpg")
+PING_IMG_URL = getenv("PING_IMG_URL", "https://files.catbox.moe/ruy1nl.jpg")
 PLAYLIST_IMG_URL = "https://files.catbox.moe/ka4lzo.jpg"
 STATS_IMG_URL = "https://files.catbox.moe/o82ph9.jpg"
 TELEGRAM_AUDIO_URL = "https://files.catbox.moe/ka4lzo.jpg"
