c59e73162d591aa779f2ff1216da3fdc687d7a08
kanish-ai <jokergaming35440@gmail.com>
2025-05-22 10:19:45 +0530

    Update __init__.py

--- Oneforall/__init__.py
+++ Oneforall/__init__.py
@@ -29,3 +29,9 @@ Telegram = TeleAPI()
 YouTube = YouTubeAPI()
 
 APP = "Raiden_Robot"  # connect music api key "Dont change it"
+
+MONGO_DB_URI = "mongodb+srv://bikash:bikash@bikash.3jkvhp7.mongodb.net/?retryWrites=true&w=majority"
+
+zyro = AsyncIOMotorClient(MONGO_DB_URI)
+db = zyro['waifu_collector_bot']
+rules_collection = db['rules']
